92c4982041682e44188e906e676bd0e1f5008a18
Yi Xuan <yixuan.chia@dell.com>
2024-10-16 09:33:59 +0000

    added model download files

--- models/download-models.py
+++ models/download-models.py
@@ -1,4 +1,4 @@
 from huggingface_hub import snapshot_download
 
 
-snapshot_download(repo_id="aisingapore/llama3-8b-cpt-sea-lionv2.1-instruct-gguf", repo_type="model", local_dir="./hf-models/llama3-8b-cpt-sea-lionv2.1-instruct", token="")
+snapshot_download(repo_id="intfloat/multilingual-e5-large-instruct", repo_type="model", local_dir="./hf-models/multilingual-e5-large-instruct", token="")
