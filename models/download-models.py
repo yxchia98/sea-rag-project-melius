f1ca70ad7c3b281b7cef619c7e9474620b84fbf8
Yi Xuan <yixuan.chia@dell.com>
2024-10-16 08:13:21 +0000

    added thingsz

--- models/download-models.py
+++ models/download-models.py
@@ -1,4 +1,4 @@
 from huggingface_hub import snapshot_download
 
 
-snapshot_download(repo_id="aisingapore/llama3-8b-cpt-sea-lionv2.1-instruct-gguf", repo_type="model", local_dir="./hf-models/llama3-8b-cpt-sea-lionv2.1-instruct", token="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")
+snapshot_download(repo_id="aisingapore/llama3-8b-cpt-sea-lionv2.1-instruct-gguf", repo_type="model", local_dir="./hf-models/llama3-8b-cpt-sea-lionv2.1-instruct", token="")
