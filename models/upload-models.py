248016d9238e7ccf830d3be1975baa8996cdf7c6
Yi Xuan <yixuan.chia@dell.com>
2024-10-23 03:05:43 +0000

    updated necessary models to download

--- models/upload-models.py
+++ models/upload-models.py
@@ -1,11 +1,11 @@
 from huggingface_hub import HfApi
 api = HfApi()
 
-api.create_repo("yixuan-chia/multilingual-e5-large-instruct-gguf", token="")
+api.create_repo("yixuan-chia/multilingual-e5-base-gguf", token="")
 
 api.upload_folder(
-    folder_path="./gguf-models/multilingual-e5-large-instruct-gguf",
-    repo_id="yixuan-chia/multilingual-e5-large-instruct-gguf",
+    folder_path="./gguf-models/multilingual-e5-base-gguf",
+    repo_id="yixuan-chia/multilingual-e5-base-gguf",
     repo_type="model",
 	token=""
 )
